7a65bc3ab475a5c08750e05f12aa4a2a05f11cee
Gideon Paul <gidzpaul95@gmail.com>
2020-12-01 00:27:59 -0500

    Refactored activity_main_screen to activity_data_screen.

--- app/src/main/java/com/gideon/fetchrewards/DataScreenActivity.kt
+++ app/src/main/java/com/gideon/fetchrewards/DataScreenActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class DataScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_data_screen)
     }
 }
