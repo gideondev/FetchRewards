95a1df2ee5eabfde3688fcc7176b7c55e2c7c152
Gideon Paul <gidzpaul95@gmail.com>
2020-11-30 23:47:53 -0500

    Added custom application.

---
+++ app/src/main/java/com/gideon/fetchrewards/app/FetchRewardsApplication.kt
@@ -0,0 +1,9 @@
+package com.gideon.fetchrewards.app
+
+import android.app.Application
+
+class FetchRewardsApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
