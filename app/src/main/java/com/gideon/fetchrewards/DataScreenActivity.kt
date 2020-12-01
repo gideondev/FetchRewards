b4114160b5e19dc301e6d5d8a46d25f0eec1d12f
Gideon Paul <gidzpaul95@gmail.com>
2020-12-01 00:27:00 -0500

    Renamed MainScreenActivity.

--- app/src/main/java/com/gideon/fetchrewards/DataScreenActivity.kt
+++ app/src/main/java/com/gideon/fetchrewards/DataScreenActivity.kt
@@ -3,7 +3,7 @@ package com.gideon.fetchrewards
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class DataScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
