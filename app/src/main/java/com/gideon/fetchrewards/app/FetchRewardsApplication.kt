0a5af06767232596983e3c748dc65b84bbbecbb2
Gideon Paul <gidzpaul95@gmail.com>
2020-11-30 23:57:06 -0500

    Koin init.

--- app/src/main/java/com/gideon/fetchrewards/app/FetchRewardsApplication.kt
+++ app/src/main/java/com/gideon/fetchrewards/app/FetchRewardsApplication.kt
@@ -1,9 +1,20 @@
 package com.gideon.fetchrewards.app
 
 import android.app.Application
+import com.gideon.fetchrewards.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class FetchRewardsApplication: Application() {
     override fun onCreate() {
         super.onCreate()
+
+        startKoin {
+            // declare used Android context
+            androidContext(this@FetchRewardsApplication)
+
+            // declare modules
+            modules(appModule)
+        }
     }
 }
---
+++ app/src/main/java/com/gideon/fetchrewards/di/AppModule.kt
@@ -0,0 +1,15 @@
+package com.gideon.fetchrewards.di
+
+
+import com.gideon.fetchrewards.app.FetchRewardsApplication
+import org.koin.android.ext.koin.androidApplication
+import org.koin.dsl.module
+
+
+val appModule = listOf(
+    module {
+        single {
+            androidApplication() as FetchRewardsApplication
+        }
+    },
+)
