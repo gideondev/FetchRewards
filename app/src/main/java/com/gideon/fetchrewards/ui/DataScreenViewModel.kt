fc84f76546f0f86a895d79545249761ec3aaf534
Gideon Paul <gidzpaul95@gmail.com>
2020-12-01 00:35:46 -0500

    Added skeleton methods.

--- app/src/main/java/com/gideon/fetchrewards/ui/DataScreenViewModel.kt
+++ app/src/main/java/com/gideon/fetchrewards/ui/DataScreenViewModel.kt
@@ -4,4 +4,11 @@ import androidx.lifecycle.ViewModel
 import com.gideon.fetchrewards.data.repositories.DataRepository
 
 class DataScreenViewModel(private val dataRepository: DataRepository) : ViewModel() {
+    init {
+        fetchData()
+    }
+
+    private fun fetchData() {
+        // TODO: Implement this.
+    }
 }
