2e6bcdd519e945276252e892e859ab2be8eb1479
Gideon Paul <gidzpaul95@gmail.com>
2020-12-01 00:23:16 -0500

    DataRepository implementation added.

---
+++ app/src/main/java/com/gideon/fetchrewards/data/repositories/DataRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.gideon.fetchrewards.data.repositories
+
+import com.gideon.fetchrewards.data.sources.base.DataRemoteSource
+import com.gideon.fetchrewards.domain.models.DataItem
+import com.gideon.fetchrewards.domain.models.Resource
+
+class DataRepositoryImpl(private val dataRemoteSource: DataRemoteSource): DataRepository {
+    override suspend fun getData(): Resource<List<DataItem>> {
+        return dataRemoteSource.getAllData()
+    }
+}
