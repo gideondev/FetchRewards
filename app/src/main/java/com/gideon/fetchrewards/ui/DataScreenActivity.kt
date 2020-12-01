d058c451f646d089ddc52e287d1653781ef6dee5
Gideon Paul <gidzpaul95@gmail.com>
2020-12-01 00:43:27 -0500

    Lazy init viewmodel in the activity.

--- app/src/main/java/com/gideon/fetchrewards/ui/DataScreenActivity.kt
+++ app/src/main/java/com/gideon/fetchrewards/ui/DataScreenActivity.kt
@@ -3,10 +3,14 @@ package com.gideon.fetchrewards.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.gideon.fetchrewards.R
+import org.koin.android.viewmodel.ext.android.viewModel
 
 class DataScreenActivity : AppCompatActivity() {
+    private val viewModel: DataScreenViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_data_screen)
+        viewModel
     }
 }
