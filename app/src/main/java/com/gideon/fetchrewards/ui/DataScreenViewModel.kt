9d7cbcaffd11d6af2963f87870a9eccfb3a1c19a
Gideon Paul <gidzpaul95@gmail.com>
2020-12-01 01:00:29 -0500

    Added a custom comparator.

--- app/src/main/java/com/gideon/fetchrewards/ui/DataScreenViewModel.kt
+++ app/src/main/java/com/gideon/fetchrewards/ui/DataScreenViewModel.kt
@@ -29,3 +29,21 @@ class DataScreenViewModel(private val dataRepository: DataRepository) : ViewMode
         }
     }
 }
+
+
+// A comparator to compare first names of Name
+class ListCustomComparator : Comparator<DataItem> {
+    override fun compare(item1: DataItem, item2: DataItem): Int {
+        return if (item1.listId == item2.listId) {
+            if (item1.name != null && item2.name != null) {
+                item1.name.compareTo(item2.name)
+            } else {
+                0
+            }
+        } else if (item1.listId!! > item2.listId!!) {
+            1
+        } else {
+            -1
+        }
+    }
+}
