054d86e159d0a717e53d0f08c25a5d210a96d9c2
Gideon Paul <gidzpaul95@gmail.com>
2020-12-01 00:38:06 -0500

    Added skeleton methods for adapter.

---
+++ app/src/main/java/com/gideon/fetchrewards/ui/DataListAdapter.kt
@@ -0,0 +1,25 @@
+package com.gideon.fetchrewards.ui
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class DataListAdapter(private val context: Context) :
+    RecyclerView.Adapter<DataListAdapter.DataListViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DataListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: DataListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+
+    inner class DataListViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    }
+}
