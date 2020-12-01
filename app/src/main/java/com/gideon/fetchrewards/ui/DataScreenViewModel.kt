b24a6e3e5dd084761bdc61d06511ee1a1b43ec43
Gideon Paul <gidzpaul95@gmail.com>
2020-12-01 00:31:23 -0500

    Added viewmodel.

---
+++ app/src/main/java/com/gideon/fetchrewards/ui/DataScreenViewModel.kt
@@ -0,0 +1,7 @@
+package com.gideon.fetchrewards.ui
+
+import androidx.lifecycle.ViewModel
+import com.gideon.fetchrewards.data.repositories.DataRepository
+
+class DataScreenViewModel(private val dataRepository: DataRepository) : ViewModel() {
+}
