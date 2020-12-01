76bcfeef92009d3efa42753652679cc36415d472
Gideon Paul <gidzpaul95@gmail.com>
2020-12-01 01:32:20 -0500

    Added documentation to app class.

--- app/src/main/java/com/gideon/fetchrewards/app/FetchRewardsApplication.kt
+++ app/src/main/java/com/gideon/fetchrewards/app/FetchRewardsApplication.kt
@@ -5,6 +5,9 @@ import com.gideon.fetchrewards.di.appModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
+/**
+ * Using custom application to initialize Koin DI.
+ */
 class FetchRewardsApplication: Application() {
     override fun onCreate() {
         super.onCreate()
